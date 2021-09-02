b015cdb4a799e833d59408bf2551aec59defbd6e
哲 徐 <151893140@qq.com>
2021-09-02 10:27:10 +0800

    Create the first Project to practice

---
+++ WebApp_MVCDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp_MVCDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
